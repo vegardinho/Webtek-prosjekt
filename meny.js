4c8e163962df5607c0902a696c41f73559476cea
Geventax <34406100+Geventax@users.noreply.github.com>
2019-10-11 13:21:50 +0100

    kult

---
+++ meny.js
@@ -0,0 +1,22 @@
+const mat = [
+    {
+        navn: "lasagne",
+        pris: 120,
+        inneholder: "Ost, pasta, kjøttdeig, tomatsaus, basilikum",
+        allergener: ["laktose"]
+    },
+
+    {
+        navn: "Vegetar burger",
+        pris: 105,
+        inneholder: "Salat, tomat, løk, basilikum",
+        allergener: ["laktose", "gluten"]
+    },
+
+    {
+        navn: "Chilli con Carne",
+        pris: 89,
+        inneholder: "Chilli, kjøttdeig, løk, bønner",
+        allergener: []
+    }
+]
