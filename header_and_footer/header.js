028a8fdb9c83a42e4360c24cbd2b9b904e4898dc
MagnusBoss <magnus.boss99@gmail.com>
2019-10-18 13:10:10 +0200

    header js ferdig

--- header_and_footer/header.js
+++ header_and_footer/header.js
@@ -1,19 +1,17 @@
-const nav = document.getElementsByClassName('nav_element')
+const nav = document.querySelectorAll('li.nav_element a')
 
 function active(page) {
     nav[page].style.color = 'white'
     nav[page].style.backgroundColor = '#ff7800'
 }
 
-
-
-const index = document.getElementById('#index_js')
-const meny = document.getElementById('#meny_js')
-const kontakt = document.getElementById('#kontakt_js')
-const om_oss = document.getElementById('#om_oss_js')
+const index = document.getElementById('index_js')
+const meny = document.getElementById('meny_js')
+const kontakt = document.getElementById('kontakt_js')
+const om_oss = document.getElementById('om_oss_js')
 
 if (document.contains(index)) {
-    console.log('HEI')
+    active(0)
 } else if (document.contains(meny)) {
     active(1)
 } else if (document.contains(om_oss)) {
