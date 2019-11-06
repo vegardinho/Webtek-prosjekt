81a8072b136d6f90a37d0b738863c23f5093c04a
Geventax <34406100+Geventax@users.noreply.github.com>
2019-11-06 14:23:54 +0100

    fuck

---
+++ mat_data.js
@@ -0,0 +1,129 @@
+const mat = [
+    {
+        navn: "Vårruller 3stk",
+        pris: 29,
+        inneholder: "Salat, gulrot, peanøtter",
+        allergener: ['nøtter'],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Kyllingvinger 3stk",
+        pris: 49,
+        inneholder: "Kyllingvinger, salatblader",
+        allergener: [],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Ostepinner m/ kremostdip",
+        pris: 49,
+        inneholder: "Ost",
+        allergener: ['laktose'],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Salat",
+        pris: 49,
+        inneholder: "Salat",
+        allergener: [],
+        ekstra: ['vegetar', 'mild']
+    },
+
+    {
+        navn: "Cheese burger",
+        pris: 119,
+        inneholder: "Kjøtt, tomat, løk, ost",
+        allergener: ['laktose', 'gluten'],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Vegetar burger",
+        pris: 105,
+        inneholder: "Salat, tomat, løk, basilikum",
+        allergener: ["laktose", "gluten"],
+        ekstra: ['vegetar', 'mild']
+    },
+
+    {
+        navn: "Chilli con Carne",
+        pris: 89,
+        inneholder: "Chilli, kjøttdeig, løk, bønner",
+        allergener: [],
+        ekstra: ['sterk']
+    },
+
+    {
+        navn: "Spaghetti Bolognese",
+        pris: 89,
+        inneholder: "Kjøttdeig, tomatsaus, løk, pasta, oregano",
+        allergener: ["gluten"],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Spaghetti carbonara",
+        pris: 99,
+        inneholder: "Fløtesaus, bacon",
+        allergener: ["laktose"],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Lasagne",
+        pris: 119,
+        inneholder: "Ost, pasta, kjøttdeig, tomatsaus, basilikum",
+        allergener: ["laktose"],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Pizza salami",
+        pris: 119,
+        inneholder: "Tomatsaus, ost, oregano, salami",
+        allergener: ["laktose", "gluten"],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Grønn curry",
+        pris: 79,
+        inneholder: "Ris, kylling, paprika, blomkål, gullrot, bambus",
+        allergener: ["gluten"],
+        ekstra: ['sterk']
+    },
+
+    {
+        navn: "Chili Beef Pasta",
+        pris: 119,
+        inneholder: "Pasta, kjøtt, chili",
+        allergener: ['gluten'],
+        ekstra: ['sterk']
+    },
+    
+    {
+        navn: "Thai curry suppe",
+        pris: 89,
+        inneholder: "Kylling, bambus, paprika, tomat",
+        allergener: [],
+        ekstra: ['sterk']
+    },
+
+    {
+        navn: "Grønnsaksboller",
+        pris: 89,
+        inneholder: "Grønnsaksboller, ris, grønnsaker",
+        allergener: [],
+        ekstra: ['mild']
+    },
+
+    {
+        navn: "Kjøttboller m/ poteter",
+        pris: 109,
+        inneholder: "Kjøtt, potet, brunsaus",
+        allergener: ['gluten', 'laktose'],
+        ekstra: ['mild']
+    }
+]
